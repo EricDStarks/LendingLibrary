c3401d8bf684abfbe4e0ef74505fcbe0e524ff1a
Eric Starks <starks.eric.d@gmail.com>
2023-08-03 09:15:54 -0500

    Add project files.

---
+++ LendingLibrary/ILibrary.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LendingLibrary
+{
+    internal interface ILibrary : IReadOnlyCollection<Book>
+    {
+        //Add book to library
+        public void Add(string title, string author);
+
+        /// <summary>
+        /// Remove a Book from the library with the given title.
+        /// </summary>
+        /// <returns>The Book, or null if not found.</returns>
+       public Book Borrow(string title);
+
+        
+        /// Return a Book to the library.
+        
+        public void Return(Book book);
+
+        public Book Search(string title);
+    }
+}
---
+++ LendingLibrary/LinkedList.cs
@@ -0,0 +1,83 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LendingLibrary
+{
+    public class LinkedList : IEnumerable<int>
+    {
+        public Node Head { get; set; }
+
+        public LinkedList()
+        {
+            Head = null;
+        }
+
+
+        public void Insert(int value)
+        {
+            Node newNode = new Node(value);
+            if (Head == null)
+            {
+                Head = newNode;
+            }
+            else
+            {
+                newNode.Next = Head;
+                Head = newNode;
+            }
+        }
+
+        public bool Includes(int value)
+        {
+            bool foundValue = false;
+            Node currentNode = Head;
+            while (currentNode != null)
+            {
+                if (currentNode.Value == value)
+                {
+                    foundValue = true;
+                }
+                currentNode = currentNode.Next;
+            }
+            return foundValue;
+        }
+
+        public string ToString()
+        {
+            string listString = "";
+            Node currentNode = Head;
+            while (currentNode != null)
+            {
+                listString += currentNode.Value + " -> ";
+                currentNode = currentNode.Next;
+            }
+            listString += "NULL";
+            return listString;
+        }
+
+        public IEnumerator<int> GetEnumerator()
+        {
+            throw new NotImplementedException();
+        }
+
+        IEnumerator IEnumerable.GetEnumerator()
+        {
+            throw new NotImplementedException();
+        }
+    }
+
+    public class Node
+    {
+        public int Value { get; set; }
+        public Node Next { get; set; }
+        public Node(int value)
+        {
+            Value = value;
+            Next = null;
+        }
+    }
+}
